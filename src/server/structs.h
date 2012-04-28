4c4725bfcdb59f172be8d0163c93c3b4ff2b2dc6
xhl_ellis <ellis_loveless@qq.com>
2012-04-28 17:09:51 +0800

    update some files ,code by donle

--- src/server/structs.h
+++ src/server/structs.h
@@ -127,6 +127,12 @@ struct JudgeStruct{
     bool time_consuming;
 };
 
+struct PhaseChangeStruct{
+    PhaseChangeStruct();
+    Player::Phase from;
+    Player::Phase to;
+};
+
 enum TriggerEvent{
     NonTrigger,
 
@@ -211,6 +217,6 @@ Q_DECLARE_METATYPE(RecoverStruct)
 Q_DECLARE_METATYPE(JudgeStar)
 Q_DECLARE_METATYPE(DamageStar)
 Q_DECLARE_METATYPE(PindianStar)
-Q_DECLARE_METATYPE(QList<int>)
+Q_DECLARE_METATYPE(PhaseChangeStruct)
 
 #endif // STRUCTS_H
