917f8aeaca6b165fe9626aea5cb78b983413e880
Ellis <ellis_loveless@qq.com>
2012-05-18 10:51:12 +0800

    fix a bug of xuanfeng

--- src/package/yjcm-package.cpp
+++ src/package/yjcm-package.cpp
@@ -508,6 +508,7 @@ public:
             if(move->from_place == Player::Equip ||
                     lingtong->tag.value("InvokeXuanfeng", false).toBool())
             {
+                lingtong->setMark("xuanfeng", 0);
                 lingtong->tag.remove("InvokeXuanfeng");
                 Room *room = lingtong->getRoom();
 
